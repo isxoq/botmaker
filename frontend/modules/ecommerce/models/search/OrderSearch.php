8e293edb9e8df9c7eca5d24cf3a911fc93f1368b
isxoq <isxoqjon_7710@mail.ru>
2021-08-04 17:16:04 +0500

    added order change statses

--- frontend/modules/ecommerce/models/search/OrderSearch.php
+++ frontend/modules/ecommerce/models/search/OrderSearch.php
@@ -69,6 +69,7 @@ class OrderSearch extends Order
         ]);
 
         $query->andFilterWhere(['like', 'comment', $this->comment]);
+        $query->andFilterWhere(['!=', 'status', self::STATUS_ORDERING]);
 
         return $dataProvider;
     }
