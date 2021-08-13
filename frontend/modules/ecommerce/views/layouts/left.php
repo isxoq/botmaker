1f6d05f32702e33b376a81bb052e1dae56aecb05
isxoq <isxoqjon_7710@mail.ru>
2021-08-13 13:37:17 +0500

    menu aktiv

--- frontend/modules/ecommerce/views/layouts/left.php
+++ frontend/modules/ecommerce/views/layouts/left.php
@@ -13,24 +13,24 @@
                                 class="menu-icon fa fa-laptop"></i><?= Yii::t('app', 'Dashboard') ?> </a>
                 </li>
 
-                <li class="<?= Yii::$app->controller->route == "/ecommerce/category/index" ? "active" : "" ?>">
+                <li class="<?= Yii::$app->controller->route == "ecommerce/category/index" ? "active" : "" ?>">
                     <a href="<?= \yii\helpers\Url::to(['/ecommerce/category/index', 'bot_id' => Yii::$app->request->get('bot_id')]) ?>"><i
                                 class="menu-icon fa fa-list"></i><?= Yii::t('app', 'Categories') ?>
                     </a>
                 </li>
-                <li class="<?= Yii::$app->controller->route == "/ecommerce/product/index" ? "active" : "" ?>">
+                <li class="<?= Yii::$app->controller->route == "ecommerce/product/index" ? "active" : "" ?>">
                     <a href="<?= \yii\helpers\Url::to(['/ecommerce/product/index', 'bot_id' => Yii::$app->request->get('bot_id')]) ?>"><i
                                 class="menu-icon fa fa-list"></i><?= Yii::t('app', 'Mahsulotlar') ?>
                     </a>
                 </li>
 
-                <li class="<?= Yii::$app->controller->route == "/ecommerce/order/index" ? "active" : "" ?>">
+                <li class="<?= Yii::$app->controller->route == "ecommerce/order/index" ? "active" : "" ?>">
                     <a href="<?= \yii\helpers\Url::to(['/ecommerce/order/index', 'bot_id' => Yii::$app->request->get('bot_id')]) ?>"><i
                                 class="menu-icon fa fa-list"></i><?= Yii::t('app', 'Buyurtmalar') ?>
                     </a>
                 </li>
 
-                <li class="<?= Yii::$app->controller->route == "/ecommerce/bot-user/index" ? "active" : "" ?>">
+                <li class="<?= Yii::$app->controller->route == "ecommerce/bot-user/index" ? "active" : "" ?>">
                     <a href="<?= \yii\helpers\Url::to(['/ecommerce/bot-user/index', 'bot_id' => Yii::$app->request->get('bot_id')]) ?>"><i
                                 class="menu-icon fa fa-list"></i><?= Yii::t('app', 'Foydalanuvchilar') ?>
                     </a>
