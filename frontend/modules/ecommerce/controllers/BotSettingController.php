5eef0d68659c2b551c500586cc36793e58d203c3
isxoq <isxoqjon_7710@mail.ru>
2021-08-16 18:40:46 +0500

    added bot delete option ficx fetch err

--- frontend/modules/ecommerce/controllers/BotSettingController.php
+++ frontend/modules/ecommerce/controllers/BotSettingController.php
@@ -14,12 +14,32 @@ namespace frontend\modules\ecommerce\controllers;
 
 use backend\modules\payme\paycom\Response;
 use frontend\models\api\TelegramBot;
+use yii\filters\Cors;
 use yii\helpers\FileHelper;
 use yii\helpers\Url;
 use yii\web\UploadedFile;
 
 class BotSettingController extends \yii\web\Controller
 {
+
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+
+        $behaviors['corsFilter'] = [
+            'class' => Cors::className(),
+            'cors' => [
+                'Origin' => ['*'],
+                'Access-Control-Request-Method' => ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+                'Access-Control-Request-Headers' => ['*'],
+                'Access-Control-Allow-Credentials' => true,
+            ],
+        ];
+
+
+        return $behaviors;
+    }
 
     public function actionIndex()
     {
