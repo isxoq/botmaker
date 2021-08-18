340eaea05efdba2dd56b49e3671eecdbb0700555
isxoq <isxoqjon_7710@mail.ru>
2021-08-18 11:43:43 +0500

    left menus fix admin

--- backend/views/layouts/left.php
+++ backend/views/layouts/left.php
@@ -4,17 +4,17 @@
         <div id="main-menu" class="main-menu collapse navbar-collapse">
             <ul class="nav navbar-nav">
                 <li class="<?= Yii::$app->controller->route == "site/index" ? "active" : "" ?>">
-                    <a href="<?= \yii\helpers\Url::to(['site/index']) ?>"><i
+                    <a href="<?= \yii\helpers\Url::to(['/site/index']) ?>"><i
                                 class="menu-icon fa fa-laptop"></i><?= Yii::t('app', 'Dashboard') ?> </a>
                 </li>
                 <li class="<?= Yii::$app->controller->route == "telegram-bot/index" ? "active" : "" ?>">
-                    <a href="<?= \yii\helpers\Url::to(['telegram-bot/index']) ?>"><i
+                    <a href="<?= \yii\helpers\Url::to(['/telegram-bot/index']) ?>"><i
                                 class="menu-icon fa fa-list"></i><?= Yii::t('app', 'Telegram Bots') ?>
                     </a>
                 </li>
 
                 <li class="<?= Yii::$app->controller->route == "translatemanager/source-message" ? "active" : "" ?>">
-                    <a href="<?= \yii\helpers\Url::to(['translatemanager/source-message']) ?>"><i
+                    <a href="<?= \yii\helpers\Url::to(['/translatemanager/source-message']) ?>"><i
                                 class="menu-icon fa fa-list"></i><?= Yii::t('app', 'Translations') ?>
                     </a>
                 </li>
